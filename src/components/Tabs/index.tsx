675af2d642b92864d743200ae2f97472c9b47866
JoaoPaulo-LD <joaopauloalencarrodrigues@gmail.com>
2020-07-20 19:37:04 -0300

    Tabs components finished

--- src/components/Tabs/index.tsx
+++ src/components/Tabs/index.tsx
@@ -1,10 +1,39 @@
 import React from 'react'
+import { MaterialIcons } from '@expo/vector-icons'
 
-import { Container } from './styles'
+import {
+  Container,
+  TabsContainer,
+  TabItem,
+  TabText
+} from './styles'
 
 const Tabs: React.FC = () => {
   return (
-    <Container />
+    <Container>
+      <TabsContainer>
+        <TabItem>
+          <MaterialIcons name="person-add" size={24} color="white" />
+          <TabText>Indicar amigos</TabText>
+        </TabItem>
+        <TabItem>
+          <MaterialIcons name="chat-bubble-outline" size={24} color="white" />
+          <TabText>Cobrar</TabText>
+        </TabItem>
+        <TabItem>
+          <MaterialIcons name="arrow-downward" size={24} color="white" />
+          <TabText>Depositar</TabText>
+        </TabItem>
+        <TabItem>
+          <MaterialIcons name="arrow-upward" size={24} color="white" />
+          <TabText>Transferir</TabText>
+        </TabItem>
+        <TabItem>
+          <MaterialIcons name="lock" size={24} color="white" />
+          <TabText>Bloquear cartão</TabText>
+        </TabItem>
+      </TabsContainer>
+    </Container>
   )
 }
 
--- src/components/Tabs/styles.ts
+++ src/components/Tabs/styles.ts
@@ -1,3 +1,30 @@
 import styled from 'styled-components/native'
+import { RectButton } from 'react-native-gesture-handler'
 
-export const Container = styled.View``
+export const Container = styled.View`
+  height: 100px;
+  margin-top: 20px;
+`
+
+export const TabsContainer = styled.ScrollView.attrs({
+  contentContainerStyle: { paddingLeft: 10, paddingRight: 20 },
+  horizontal: true,
+  showsHorizontalScrollIndicator: false
+})``
+
+export const TabItem = styled(RectButton).attrs({
+  rippleColor: "rgba(255,255,255,0.2)"
+})`
+  width: 100px;
+  height: 100px;
+  background: rgba(255,255,255,0.2);
+  border-radius: 3px;
+  margin-left: 10px;
+  padding: 10px;
+  justify-content: space-between;
+`
+
+export const TabText = styled.Text`
+  font-size: 13px;
+  color: white
+`
