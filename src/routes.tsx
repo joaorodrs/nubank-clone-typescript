79b25db51bc45143464f0ec54c658422121467c1
JoaoPaulo-LD <joaopauloalencarrodrigues@gmail.com>
2020-07-20 18:38:15 -0300

    Added and finished Router file

---
+++ src/routes.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import { createStackNavigator } from '@react-navigation/stack'
+import Main from './Main'
+
+const AppStack = createStackNavigator()
+
+const Routes = () => {
+  return (
+    <AppStack.Navigator>
+      <AppStack.Screen name="Main" component={Main} />
+    </AppStack.Navigator>
+  )
+}
+
+
+export default Routes
